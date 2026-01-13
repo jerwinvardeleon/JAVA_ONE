24698b19b05878b1474eaed2f43f70c844ebc504
Jer Vardeleon <jerwin.vardeleon@gmail.com>
2026-01-14 04:45:15 +0800

    Version1

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("asdad")
+console.log("Asdasd")
+console.log("ssss")
+console.log("HI HI")
+console.log("Jerwin")
+console.log(`JErwin`.length);
+let string = "JERWIN"
+console.log(string[string.length-1])
