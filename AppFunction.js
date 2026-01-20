549c66b66332948c7db86572742f25f9146f05db
Jer <jerwin.vardeleon@gmail.com>
2026-01-20 18:35:07 +0800

    version.3.1

--- AppFunction.js
+++ AppFunction.js
@@ -6,3 +6,20 @@ function greetings(name, surname){
 // Calling the function
 greetings("Jer","Vardeleon");
 greetings("Kat","Banas");
+
+
+
+//EXAMPLE 1
+function sum(num1, num2){
+  return num1 + num2
+}
+console.log(sum(2,4))
+
+
+// EXAMPLE 2
+function convert(USD){
+    console.log("AUD is " + USD * 1.5);
+}
+convert(1000)
+convert(500)
+convert(200)
