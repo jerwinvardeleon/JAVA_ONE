b920b893d3071ff856b0fc6a6607b88a58bfcc15
Jer <jerwin.vardeleon@gmail.com>
2026-01-20 18:19:36 +0800

    version.3

---
+++ AppFunction.js
@@ -0,0 +1,8 @@
+//Function Declaration
+function greetings(name, surname){
+    console.log("Hello, " + name + " " + surname);
+}
+
+// Calling the function
+greetings("Jer","Vardeleon");
+greetings("Kat","Banas");
